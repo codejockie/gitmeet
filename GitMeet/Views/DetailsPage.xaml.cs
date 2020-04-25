5c7660c458c6fad412b81dcaaba8f6bb77a35df3
John C. Kennedy <devjckennedy@gmail.com>
2020-04-25 16:45:15 +0900

    implement details page

---
+++ GitMeet/Views/DetailsPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GitMeet.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailsPage : ContentPage
+    {
+        public DetailsPage()
+        {
+            InitializeComponent();
+
+            NavigationPage.SetBackButtonTitle(this, "Return");
+        }
+
+        void OnPageSizeChanged(object sender, EventArgs args)
+        {
+            // Portrait mode. 
+            if (Width < Height)
+            {
+                mainGrid.RowDefinitions[1].Height = GridLength.Auto;
+                mainGrid.ColumnDefinitions[1].Width = new GridLength(0, GridUnitType.Absolute);
+
+                Grid.SetRow(detailsStack, 1);
+                Grid.SetColumn(detailsStack, 0);
+            }
+            // Landscape mode. 
+            else
+            {
+                mainGrid.RowDefinitions[1].Height = new GridLength(0, GridUnitType.Absolute);
+                mainGrid.ColumnDefinitions[1].Width = new GridLength(1, GridUnitType.Star);
+
+                Grid.SetRow(detailsStack, 0);
+                Grid.SetColumn(detailsStack, 1);
+            }
+        }
+
+        [Obsolete]
+        void OnUrlLabelTapped(object sender, EventArgs args)
+        {
+            var url = ((Label)sender).Text;
+
+            Device.OpenUri(new Uri(url));
+        }
+    }
+}
